b036c2cd7161ccef5f858703304c835de53ecf3c
magom <magomlg@gmail.com>
2022-12-20 10:34:47 -0500

    Nuevo trabajo

---
+++ src/com/diseno/ralv/main/main.java
@@ -0,0 +1,25 @@
+package com.diseno.ralv.main;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Entraga de trabajo");
+
+        int numero = 1;
+        long numLong = 12345678;
+        double numDouble = 25.5;
+        boolean bool = false;
+        String palabra = "Hola mundo";
+
+
+        System.out.println("Entero es :" + numero +
+                           "\nLong es: " + numLong  +
+                           "\nDoble es: " + numDouble +
+                           " \nBoolenano es: " + bool +
+                           "\nCadema es : " + palabra);
+
+
+    }
+
+}
